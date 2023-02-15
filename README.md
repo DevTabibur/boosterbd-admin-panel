7a4bbf5f4f69087e261996c1f4d678fdf3e32514
unknown <tobiburrohman2@gmail.com>
2023-02-15 21:21:42 +0600

    reset-password done

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
+## Completed
+
+1. Registration
+2. Login
 ## Problems
 
 1. http://localhost:3001/users => ei route e gele, network tab ee, automatic data refetch and render hotei thake. jar jonne pc sound kortese. etar alternative khujte hobe.
 2. Loader ekhono thik vabe set kora hoini
+3. nodemailer ee node-mailer-pass deya lagbe.... site tar admin er email deya lagbe pore.
---
+++ src/pages/forget-password/index.js
@@ -0,0 +1,120 @@
+import { useForm } from "react-hook-form";
+
+import Swal from "sweetalert2";
+import { useState } from "react";
+import Loader from "@/Components/Shared/Loader/Loader";
+import { useRouter } from "next/router";
+
+const index = () => {
+  const router = useRouter();
+  const [resetLoading, setResetLoading] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = async (data, e) => {
+    const email = data.email;
+    console.log("email", email);
+
+    const url = `http://localhost:5000/api/v1/forgot-password`;
+    setResetLoading(true);
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({ email: email }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("data posted", data);
+        setResetLoading(false);
+        if (data.code === 401 || data.code === 402 || data.code === 403) {
+          Swal.fire({
+            title: data?.status,
+            text: data?.error,
+            icon: "error",
+          });
+        } else {
+          Swal.fire({
+            title: " Email was sent!",
+            text: "Check your Email, for reset",
+            icon: "success",
+          });
+          router.push("/");
+        }
+      });
+  };
+
+  const goBack = () => {
+    return router.push("/");
+  };
+
+  if (resetLoading) {
+    return <Loader />;
+  }
+  return (
+    <>
+      <div class="flex justify-center items-center h-screen">
+        <div class="rounded-lg shadow-lg bg-red-100 w-96 p-8">
+          <h1 className="text-accent text-mono font-bold text-3xl mb-4">
+            Forgot Password?
+          </h1>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="form-group mb-6">
+              <input
+                type="text"
+                placeholder="Enter Your Email"
+                class="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                {...register("email", {
+                  required: {
+                    value: true,
+                    message: "Email is Required",
+                  },
+                  pattern: {
+                    value: /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/,
+                    message: "Provide a valid Email",
+                  },
+                })}
+              />
+              <label className="label my-1 py-0">
+                {errors.email?.type === "required" && (
+                  <span className="label-text-alt text-red-500 ">
+                    {errors.email.message}
+                  </span>
+                )}
+                {errors.email?.type === "pattern" && (
+                  <span className="label-text-alt text-red-500 ">
+                    {errors.email.message}
+                  </span>
+                )}
+              </label>
+            </div>
+
+            <div className="buttons py-3">
+              <button
+                onClick={goBack}
+                type="button"
+                className="border btn-1 border-[#465EED] bg-[#465EED] text-white rounded-xl px-10 py-2  transition duration-500 ease select-none hover:bg-[#465EED] focus:outline-none focus:shadow-outline"
+              >
+                Go Back
+              </button>
+              <button>
+                <input
+                  className="border btn-1 ml-4 border-[#465EED] bg-[#465EED] text-white rounded-xl px-10 py-2  transition duration-500 ease select-none hover:bg-[#465EED] focus:outline-none focus:shadow-outline"
+                  type="submit"
+                  value="RESET"
+                ></input>
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default index;
